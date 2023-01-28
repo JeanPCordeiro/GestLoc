da9bbd9fd9103748267a69c06d1cf58547eceaef
JeanPCordeiro <jeanpierre.cordeiro@gmail.com>
2023-01-28 18:47:43 +0000

    Install filament

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://8000-jeanpcordeiro-gestloc-62r4lttmstl.ws-eu84.gitpod.io'),
 
     'asset_url' => env('ASSET_URL'),
 
